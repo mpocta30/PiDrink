d54622f01618f7b9e0b9aba1c3fb78d2bea25d07
mpocta30 <40032123+mpocta30@users.noreply.github.com>
2018-12-04 11:50:00 -0500

    Update relaycontrol.py

--- relaycontrol.py
+++ relaycontrol.py
@@ -13,11 +13,11 @@ pins = [5, 6, 13, 19, 26, 16]
 
 for count in range(0, 5):
   for i in range(0, 5):
-    GPIO.output(pins[i], GPIO.LOW) // energize relays until all on
+    GPIO.output(pins[i], GPIO.LOW) # energize relays until all on
     sleep(1)
 
   for i in range(0, 5):
-    GPIO.output(pins[i], GPIO.HIGH) // de-energize relays until all off
+    GPIO.output(pins[i], GPIO.HIGH) # de-energize relays until all off
     sleep(1)
 
 GPIO.cleanup()
