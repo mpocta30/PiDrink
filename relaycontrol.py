d99c15d48db4e09a3369fb3e43234bf708ead645
mpocta30 <40032123+mpocta30@users.noreply.github.com>
2018-12-04 11:35:50 -0500

    Create relaycontrol.py

---
+++ relaycontrol.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(5, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(6, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(13, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(19, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(26, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(16, GPIO.OUT, initial=GPIO.HIGH)
+
+pins = [5, 6, 13, 19, 26, 16]
+
+for count in range(0, 5):
+  for i in range(0, 5):
+    GPIO.output(pins[i], GPIO.LOW) // energize relays until all on
+    sleep(1)
+
+  for i in range(0, 5):
+    GPIO.output(pins[i], GPIO.HIGH) // de-energize relays until all off
+    sleep(1)
+
+GPIO.cleanup()
