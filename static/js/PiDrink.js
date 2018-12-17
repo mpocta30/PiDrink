a8f3b7a1a37256fa72747f74158d7b8d5107263d
mpocta30 <mpocta30@vt.edu>
2018-12-16 20:54:17 -0500

    Fix file paths

--- static/js/PiDrink.js
+++ static/js/PiDrink.js
@@ -115,7 +115,7 @@ $( document ).ready(function() {
         // Prevent page refresh
         e.preventDefault();
 
-        alert( $( this ).serialize() );
+        alert($('#createdrink').serialize());
     });
 
     
