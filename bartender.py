870984d2329a1323eaae78a29316c79b03b52620
mpocta30 <mpocta30@vt.edu>
2018-12-18 16:46:40 -0500

    Goodbye message when program is stopped

--- bartender.py
+++ bartender.py
@@ -504,6 +504,13 @@ class Bartender(MenuDelegate):
 	def endprogram(self):
 		global stopprogram
 
+		# Goodbye message
+		self.draw.rectangle((0,0,SCREEN_WIDTH, SCREEN_HEIGHT),outline=0,fill=0)
+		self.draw.text((20,10), "Goodbye", font=self.font, fill=255)
+		self.draw.text((20,20), "Have a great day!", font=self.font, fill=255)
+		self.disp.image(self.image)
+		self.disp.display()
+
 		stopprogram = True
 
 
