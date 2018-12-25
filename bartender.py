3241036c8c16ba33dde8e92c58a8b1e0dc781e89
mpocta30 <mpocta30@vt.edu>
2018-12-25 00:23:10 -0500

    Fix sudo issues

--- bartender.py
+++ bartender.py
@@ -107,16 +107,16 @@ class Bartender(MenuDelegate):
  		# Change rows & cols values depending on your display dimensions.
 
 		# load the pump configuration from file
-		self.pump_configuration = Bartender.readJson('/Documents/PiDrink/static/json/pump_config.json')
+		self.pump_configuration = Bartender.readJson('/Documents/PiDrink//home/pi/Documents/PiDrink/static/json/pump_config.json')
 		for pump in self.pump_configuration.keys():
 			GPIO.setup(self.pump_configuration[pump]["pin"], GPIO.OUT, initial=GPIO.HIGH)
 
 		# load the current drink list
-		self.drink_list = Bartender.readJson('/Documents/PiDrink/static/json/drink_list.json')
+		self.drink_list = Bartender.readJson('/Documents/PiDrink//home/pi/Documents/PiDrink/static/json/drink_list.json')
 		self.drink_list = self.drink_list["drink_list"]
 
 		# load the current drink options
-		self.drink_options = Bartender.readJson('/Documents/PiDrink/static/json/drink_options.json')
+		self.drink_options = Bartender.readJson('/Documents/PiDrink//home/pi/Documents/PiDrink/static/json/drink_options.json')
 		self.drink_options = self.drink_options["drink_options"]
 
 		# setup pixels:
@@ -174,7 +174,7 @@ class Bartender(MenuDelegate):
 
 	@staticmethod
 	def writePumpConfiguration(configuration):
-		with open("/Documents/PiDrink/static/json/pump_config.json", "w") as jsonFile:
+		with open("/Documents/PiDrink//home/pi/Documents/PiDrink/static/json/pump_config.json", "w") as jsonFile:
 			json.dump(configuration, jsonFile)
 
 	def startInterrupts(self):
